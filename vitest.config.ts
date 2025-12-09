c954d7e9b0832ca9ded68a94ccddf4621fdb829e
Giri Babu Devalla <giribabu.devalla@zensar.com>
2025-12-09 20:18:01 +0530

    fix test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    environment:'jsdom',
     coverage: {
-      enabled: true,
+       enabled: true,
        provider: 'istanbul',
+       reporter: ['lcov','json-summary', 'html'],
        include: ['src/**/*.{ts,tsx}']
-    },
-  },
+    }
+  }
 })
